3d34bf41e03f34798a3e955d2076908877786b0a
asymmetric <asymmetric@autistici.org>
2010-06-18 11:56:06 +0200

    Added application
    There are both an Erb and a HAML template.
    I'm leaning towards HAML atm.

---
+++ soundcloud.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'less'
+
+set :app_file, __FILE__
+
+get '/' do
+	haml :main
+end
+
+post '/send' do
+	unless params[:file]
+		@error = "No file selected"
+	else
+		File.new params[:file][:filename], 'w+'
+		"Uploaded #{params[:file][:filename]}"
+	end
+end
+
+get '/send' do
+	erb :main
+end
+
+get '/views/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  less :style
+end
+
+helpers do
+	def my_send filename
+		"sending #{filename}"
+	end
+end
+
+not_found do
+	'Sorry mate!'
+end
