d705aa8f50ead8f7dba85a36378d73294fd97a43
asymmetric <asymmetric@autistici.org>
2010-06-19 18:34:06 +0200

    Added event handler for submit's onclick

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -1,6 +1,28 @@
+function init() {
+  change_target();
+  add_events();
+}
+
 function change_target() {
   var form = document.getElementById('fileform');
   form.setAttribute('target', 'upload_iframe');
 }
 
-window.onload = change_target;
+function add_events() {
+  var submit = document.getElementById('sendbutton');
+  submit.onclick = function() {
+    var progressbar = document.getElementById('progressbar');
+    progressbar.style.display = '';
+  }
+}
+
+function display_element() {
+  var submit = document.getElementById('sendbutton');
+  var iframe = document.getElementById('upload_iframe');
+  submit.style.display = "";
+  this.style.display = "";
+}
+
+
+
+window.onload = init;
